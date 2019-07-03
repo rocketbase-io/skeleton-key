d7c761fa3db7c4afceeeac76adfe876c6341e58e
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2019-07-03 16:30:55 +0200

    typo fix

--- src/request-interceptor.ts
+++ src/request-interceptor.ts
@@ -56,7 +56,7 @@ export function interceptXHRSend() {
     // @ts-ignore
     const body = requestSendMiddleware.apply(this, arguments);
     // @ts-ignore
-    if (args != null) return orig.call(this, body);
+    if (body != null) return orig.call(this, body);
   };
   // @ts-ignore
   send.__intercepted = true;

36a6878175d64693dda751ec8f144f3f149c9c5e
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-08-14 13:58:04 +0200

    add unit tests for interceptFunction and installInterceptors

---
+++ test/intercept/intercept.test.ts
@@ -0,0 +1,80 @@
+import "jasmine";
+import {installInterceptors, interceptFunction} from "../../src/intercept";
+
+
+describe("intercept", () => {
+
+  describe("intercept", () => {
+
+    describe("#interceptFunction()", () => {
+
+      it("should replace a function with an intercepted copy", () => {
+        const obj = { toBeIntercepted(...args) { return args } };
+        const spy = jasmine.createSpy();
+
+        interceptFunction(obj, "toBeIntercepted", spy);
+
+        expect((obj.toBeIntercepted as any).__intercepted).toBeTruthy();
+      });
+
+      it("should forward this context and arguments to the interceptor", () => {
+        const obj = { toBeIntercepted(...args) { return args } };
+        const spy = jasmine.createSpy("toBeIntercepted", function() {
+          expect(this).toBe(obj);
+        }).and.callThrough();
+
+        interceptFunction(obj, "toBeIntercepted", spy);
+
+        obj.toBeIntercepted("do", "the", "thing");
+
+        expect(spy).toHaveBeenCalledWith("do", "the", "thing");
+
+      });
+
+      it("should cancel execution if the interceptor throws an exception", () => {
+        const obj = { toBeIntercepted(...args) { return args } };
+        const spy = jasmine.createSpy("toBeIntercepted", function() {
+          throw new Error("The Error");
+        }).and.callThrough();
+
+        interceptFunction(obj, "toBeIntercepted", spy);
+
+        const result = obj.toBeIntercepted("do", "the", "thing");
+
+        expect(spy).toHaveBeenCalledWith("do", "the", "thing");
+        expect(result).toBeUndefined();
+      });
+
+      it("should pass the returned arguments of the middleware to the original function", () => {
+        const obj = { toBeIntercepted(...args) { return args } };
+        const spy = jasmine.createSpy("toBeIntercepted", function() {
+          expect(this).toBe(obj);
+          return ["baby", "don't", "hurt", "me"];
+        }).and.callThrough();
+
+        interceptFunction(obj, "toBeIntercepted", spy);
+
+        const result = obj.toBeIntercepted("what", "is", "love");
+
+        expect(spy).toHaveBeenCalledWith("what", "is", "love");
+        expect(result).toEqual(["baby", "don't", "hurt", "me"]);
+      });
+
+    });
+
+    describe("#installInterceptors()", () => {
+
+      it("intercepts xhr and fetch methods", () => {
+        installInterceptors();
+        const xhr = XMLHttpRequest.prototype as any;
+        expect(xhr.open.__intercepted).toBeTruthy();
+        expect(xhr.send.__intercepted).toBeTruthy();
+        expect(xhr.setRequestHeader.__intercepted).toBeTruthy();
+        expect((fetch as any).__intercepted).toBeTruthy();
+      });
+
+    });
+
+  });
+
+});

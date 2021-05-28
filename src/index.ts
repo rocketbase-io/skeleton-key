ab01a8061e19b04717a2d3941c1f32b9d1a2e2d9
Merlin Reichwald <merlin.reichwald@gmail.com>
2021-05-28 15:30:18 +0200

    fix: await openid action before closing window

--- src/index.ts
+++ src/index.ts
@@ -304,8 +304,8 @@ export class SkeletonKey<USER_DATA = unknown, TOKEN_DATA = unknown>
       if (code === false) continue;
       shouldClose = typeof shouldClose === "function" ? shouldClose(action) : shouldClose || action.state === "close";
       try {
-        if (action.login && typeof code === "string") return this.openIdLogin(code, action);
-        if (action.logout) return this.openIdLogout();
+        if (action.login && typeof code === "string") return await this.openIdLogin(code, action);
+        if (action.logout) return await this.openIdLogout();
       } finally {
         if (shouldClose && typeof close !== "undefined") close();
       }
